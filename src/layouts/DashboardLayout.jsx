5d0cc780e1daca108c1eb76ae14db15cf46a9e24
SDNATH-Git <sd.nath1952@gmail.com>
2025-12-03 02:22:26 +0600

    added dashboard

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -10,12 +10,12 @@ const DashboardLayout = () => {
     const { role, roleLoading } = useUserRole();
 
     return (
-        <div className="drawer lg:drawer-open">
+        <div className="drawer lg:drawer-open  ">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col">
+            <div className="drawer-content flex flex-col ">
 
                 {/* Navbar */}
-                <div className="navbar bg-base-300 w-full lg:hidden">
+                <div className="navbar bg-base-300 w-full lg:hidden shadow-orange-500 ">
                     <div className="flex-none ">
                         <label htmlFor="my-drawer-2" aria-label="open sidebar" className="btn btn-square btn-ghost">
                             <svg
@@ -41,9 +41,9 @@ const DashboardLayout = () => {
                 {/* Page content here */}
 
             </div>
-            <div className="drawer-side">
+            <div className="drawer-side ">
                 <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-                <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                <ul className="menu bg-base-200 text-base-content min-h-full pr-4 p-4 mr-6 ">
                     {/* Sidebar content here */}
                     <ProFastLogo></ProFastLogo>
                     <li>
--- src/pages/shared/ProFastLogo/ProFastLogo.jsx
+++ src/pages/shared/ProFastLogo/ProFastLogo.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router';
 const ProFastLogo = () => {
     return (
         <Link to="/">
-            <div className='flex items-start '>
+            <div className='flex items-start border-b-4 border-[#F04C2B] mb-4 '>
                 <img className='mb-2 w-[200px]' src={logo} alt="" />
                 {/* <p className='text-3xl -ml-2 font-extrabold'>ProFast</p> */}
             </div>
