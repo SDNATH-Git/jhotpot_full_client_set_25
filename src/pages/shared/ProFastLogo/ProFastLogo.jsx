26e810755b2438ba7e3b35b72b9d0cdb05c2cd05
SDNATH-Git <sd.nath1952@gmail.com>
2025-12-03 01:57:22 +0600

    added logo

--- src/pages/shared/ProFastLogo/ProFastLogo.jsx
+++ src/pages/shared/ProFastLogo/ProFastLogo.jsx
@@ -5,9 +5,9 @@ import { Link } from 'react-router';
 const ProFastLogo = () => {
     return (
         <Link to="/">
-            <div className='flex items-end'>
-                <img className='mb-2' src={logo} alt="" />
-                <p className='text-3xl -ml-2 font-extrabold'>ProFast</p>
+            <div className='flex items-start '>
+                <img className='mb-2 w-[200px]' src={logo} alt="" />
+                {/* <p className='text-3xl -ml-2 font-extrabold'>ProFast</p> */}
             </div>
         </Link>
     );
