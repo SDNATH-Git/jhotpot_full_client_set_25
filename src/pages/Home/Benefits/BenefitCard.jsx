3d4d217a13d31638be20f811da68225c77551caa
SDNATH-Git <sd.nath1952@gmail.com>
2026-01-01 23:39:51 +0600

    update show card

--- src/pages/Home/Benefits/BenefitCard.jsx
+++ src/pages/Home/Benefits/BenefitCard.jsx
@@ -13,7 +13,7 @@ const BenefitCard = ({ title, description, image, index }) => {
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
       transition={{ duration: 0.6, delay: index * 0.2 }}
-      className={`group flex flex-col items-center text-center p-6 rounded-3xl shadow-md hover:shadow-xl transition-all duration-300 border border-transparent hover:border-[#0D5EA6]/50 ${gradientBg}`}
+      className={`group flex flex-col items-center text-center p-6 rounded-3xl shadow-md hover:shadow-xl transition-all duration-300 border-t-4 border-b-2 border-[#0D5EA6]  hover:border-[#0D5EA6]/50 ${gradientBg}`}
     >
       <motion.img
         whileHover={{ scale: 1.1 }}
