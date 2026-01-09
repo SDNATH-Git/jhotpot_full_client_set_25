063d889cfc09dec0aa8b731b9987081e74cd8531
SDNATH-Git <sd.nath1952@gmail.com>
2026-01-10 00:04:16 +0600

    added loading new color

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -26,7 +26,7 @@ const Loading = () => {
             {/* DaisyUI spinner for extra visual feedback */}
             {/* <span className="loading loading-spinner loading-lg text-[#F04C2B] mt-6"></span> */}
             <div className="flex justify-center items-center  mt-6">
-                <FaSpinner className="text-4xl animate-spin text-[#0D5EA6]" />
+                <FaSpinner className="text-4xl animate-spin text-[#F04C2B]" />
                 <p className="text-lg text-gray-600 ml-3">Loading...</p>
             </div>
         </div>
